64c702edbfb5fa92474930cd63734272bb76e299
Radolina Petrova <478897@student.fontys.nl>
2024-03-20 20:18:47 +0100

    Test pipeline separation

--- post_service/src/main/java/com/veggietalk/post_service/persistence/impl/PostRepoImpl.java
+++ post_service/src/main/java/com/veggietalk/post_service/persistence/impl/PostRepoImpl.java
@@ -2,6 +2,6 @@ package com.veggietalk.post_service.persistence.impl;
 
 public class PostRepoImpl {
     public String Test(){
-        return "_|_ :) :D XD";
+        return "_|_ :) :D XD :((((";
     }
 }
