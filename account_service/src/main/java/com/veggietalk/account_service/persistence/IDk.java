4cd6acaed2ec94831a4312874bd935fa781788cb
Radolina Petrova <478897@student.fontys.nl>
2024-03-22 11:39:27 +0100

    #3

--- account_service/src/main/java/com/veggietalk/account_service/persistence/IDk.java
+++ account_service/src/main/java/com/veggietalk/account_service/persistence/IDk.java
@@ -2,6 +2,6 @@ package com.veggietalk.account_service.persistence;
 
 public class IDk {
     private String hehe(){
-        return ":(((_:(((((LKNLKNLKkjlknkn;k;j;lm';klnn;ljnkkm;;nlklkpo;l";
+        return ":(((_:(((((LKNLKNLKkjlknkn;k;j;lm';klnn;ljnkkm;;nlklkpo;l;l;l";
     }
 }
