752f492647547e5adc92065df1c321a5848cef8f
Radolina Petrova <478897@student.fontys.nl>
2024-03-25 00:04:00 +0100

    unit tests

--- post_service/src/test/java/com/veggietalk/post_service/service_impl/controllerTests/PostControllerTest.java
+++ post_service/src/test/java/com/veggietalk/post_service/service_impl/controllerTests/PostControllerTest.java
@@ -1,18 +1,12 @@
 package com.veggietalk.post_service.service_impl.controllerTests;
 
-import com.veggietalk.post_service.controller.PostController;
 import com.veggietalk.post_service.controller.converters.RequestConverters;
 import com.veggietalk.post_service.controller.requests.CreatePostRequest;
 import com.veggietalk.post_service.domain.Post;
-import com.veggietalk.post_service.service.PostService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
