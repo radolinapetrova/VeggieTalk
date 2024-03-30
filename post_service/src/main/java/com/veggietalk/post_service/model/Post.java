e4c718a56aec84b12387b415a5a8e08c339d8f1c
Radolina Petrova <478897@student.fontys.nl>
2024-03-30 16:05:01 +0100

    #8

--- post_service/src/main/java/com/veggietalk/post_service/model/Post.java
+++ post_service/src/main/java/com/veggietalk/post_service/model/Post.java
@@ -2,8 +2,11 @@ package com.veggietalk.post_service.model;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Builder
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
@@ -11,4 +14,15 @@ public class Post {
     private String date;
     private Long userId;
     private String description;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) &&
+                Objects.equals(date, post.date) &&
+                Objects.equals(userId, post.userId) &&
+                Objects.equals(description, post.description);
+    }
 }
