b4d6409b3a49b7d89bd133a2a37c718be57d69bc
Radolina Petrova <478897@student.fontys.nl>
2024-06-06 03:38:47 +0200

    tests fix

--- post_service/src/test/java/com/veggietalk/post_service/service_impl/serviceTests/PostServiceImplTest.java
+++ post_service/src/test/java/com/veggietalk/post_service/service_impl/serviceTests/PostServiceImplTest.java
@@ -1,6 +1,9 @@
 package com.veggietalk.post_service.service_impl.serviceTests;
 
+import com.veggietalk.post_service.model.Category;
+import com.veggietalk.post_service.model.DifficultyLevel;
 import com.veggietalk.post_service.model.Post;
+import com.veggietalk.post_service.model.Recipe;
 import com.veggietalk.post_service.persistence.PostRepo;
 import com.veggietalk.post_service.service.impl.PostServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -127,6 +130,41 @@ class PostServiceImplTest {
 
         //ASSERT
         verify(postRepo, times(1)).getAllPosts(page);
+    }
+
+
+    @Test
+    void getAllRecipesByDifficulty(){
+        //ARRANGE
+        List<Post> posts = List.of(
+                Post.builder().id(UUID.fromString("123e4567-e89b-12d3-a456-426614174000")).date(currentDate.format(formatter)).description("Descr").accountId(UUID.fromString("123e4567-e89b-12d3-a456-426614174000")).recipe(Recipe.recipeBuilder().difficultyLevel(DifficultyLevel.HARD).category(Category.DINNER).ingredients(List.of("ffood")).build()).build(),
+                Post.builder().id(UUID.fromString("550e8400-e29b-41d4-a716-446655440000")).date(currentDate.format(formatter)).description("Descr2").accountId(UUID.fromString("550e8400-e29b-41d4-a716-446655440000")).recipe(Recipe.recipeBuilder().difficultyLevel(DifficultyLevel.HARD).category(Category.DINNER).ingredients(List.of("ffood")).build()).build(),
+                Post.builder().id(UUID.fromString("e3b0c442-98fc-1c14-9af4-8b2b2ef8b6cd")).date(currentDate.format(formatter)).description("Descr3").accountId(UUID.fromString("e3b0c442-98fc-1c14-9af4-8b2b2ef8b6cd")).recipe(Recipe.recipeBuilder().difficultyLevel(DifficultyLevel.HARD).category(Category.DINNER).ingredients(List.of("ffood")).build()).build()
+        );
+        //ACT
+        when(postRepo.findAllRecipesByDifficulty(DifficultyLevel.HARD)).thenReturn(posts);
+        List<Post> result = service.findAllRecipesByDifficulty(DifficultyLevel.HARD);
+
+        //ASSERT
+        verify(postRepo, times(1)).findAllRecipesByDifficulty(DifficultyLevel.HARD);
+        assertEquals(result.get(1).getRecipe().getDifficultyLevel(), DifficultyLevel.HARD);
+    }
+
+    @Test
+    void getAllRecipesByCategory(){
+        //ARRANGE
+        List<Post> posts = List.of(
+                Post.builder().id(UUID.fromString("123e4567-e89b-12d3-a456-426614174000")).date(currentDate.format(formatter)).description("Descr").accountId(UUID.fromString("123e4567-e89b-12d3-a456-426614174000")).recipe(Recipe.recipeBuilder().difficultyLevel(DifficultyLevel.HARD).category(Category.DINNER).ingredients(List.of("ffood")).build()).build(),
+                Post.builder().id(UUID.fromString("550e8400-e29b-41d4-a716-446655440000")).date(currentDate.format(formatter)).description("Descr2").accountId(UUID.fromString("550e8400-e29b-41d4-a716-446655440000")).recipe(Recipe.recipeBuilder().difficultyLevel(DifficultyLevel.HARD).category(Category.DINNER).ingredients(List.of("ffood")).build()).build(),
+                Post.builder().id(UUID.fromString("e3b0c442-98fc-1c14-9af4-8b2b2ef8b6cd")).date(currentDate.format(formatter)).description("Descr3").accountId(UUID.fromString("e3b0c442-98fc-1c14-9af4-8b2b2ef8b6cd")).recipe(Recipe.recipeBuilder().difficultyLevel(DifficultyLevel.HARD).category(Category.DINNER).ingredients(List.of("ffood")).build()).build()
+        );
+        //ACT
+        when(postRepo.findAllRecipesByCategory(Category.DINNER)).thenReturn(posts);
+        List<Post> result = service.findAllRecipesByCategory(Category.DINNER);
+
+        //ASSERT
+        verify(postRepo, times(1)).findAllRecipesByCategory(Category.DINNER);
+        assertEquals(result.get(1).getRecipe().getCategory(), Category.DINNER);
     }
 
 }
