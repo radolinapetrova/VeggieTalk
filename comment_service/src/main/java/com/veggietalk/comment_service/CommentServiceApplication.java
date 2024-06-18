627c380e66467222390452b34ed000853f5b12bf
Radolina Petrova <478897@student.fontys.nl>
2024-06-18 21:24:52 +0200

    minor change

--- comment_service/src/main/java/com/veggietalk/comment_service/CommentServiceApplication.java
+++ comment_service/src/main/java/com/veggietalk/comment_service/CommentServiceApplication.java
@@ -2,10 +2,8 @@ package com.veggietalk.comment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CommentServiceApplication {
 
     public static void main(String[] args) {
